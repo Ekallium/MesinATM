1823c5a793f6a70db83c16a3e3b66e0d9c0c2f5a
Moch. Haikal Putra Muhajir <141520793+Ekallium@users.noreply.github.com>
2023-09-06 10:42:22 +0700

    Update TemplateATM.java

--- TemplateATM.java
+++ TemplateATM.java
@@ -2,24 +2,42 @@ import java.util.Scanner;
     public class TemplateATM {
         public static void main(String[] args){
             Scanner scan = new Scanner(System.in);
-            int saldo = 4575000;
+            int saldo = 4575000 ;
+            int uang = 500000;
             System.out.println("---------------------------------");
             System.out.println("  SELAMAT DATANG DI BANK JALI !");
             System.out.println("---------------------------------");
+            boolean exit = false;
         
-            while (true){
+            while (!exit){
                 System.out.println("PILIHAN MENU");
                 System.out.println("1. Cek Saldo");
                 System.out.println("2. Tarik Tunai");
                 System.out.println("3. Setor Tunai");
                 System.out.println("4. Keluar");
                 int choice = scan.nextInt();
+                scan.nextLine();
                 switch (choice){
                     case 1:
-                        System.out.println("Saldo Anda Tersisa : Rp." + saldo);
+                    case 2:            
+                    case 3:
+                    case 4:
                     default :
-                        System.out.println("Dimohon masukkan angka yang terdapat pada pilihan");
+                        System.out.println("Dimohon masukkan angka yang terdapat pada pilihan diatas");
+                }   
+                System.out.println("Apakah anda ingin melakukan transaksi lain Y/T"); 
+                    char transaksi = scan.next().charAt(0);
+                    if (!exit){
+                    if (transaksi =='t' || transaksi == 'T' ){
+                    System.out.println("Terimakasih Telah menggunakan Bank Jali");
+                    exit = true;
+                    }  
+                    else if (transaksi =='y' || transaksi == 'Y'){
+
+                        exit = false;
+                    }
                 }
             }
+
+            }
         }
-    }
