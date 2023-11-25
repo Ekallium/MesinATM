e3f455796041503872730ce7f1004a1e22d43478
Moch. Haikal Putra Muhajir <141520793+Ekallium@users.noreply.github.com>
2023-11-25 16:14:56 +0700

    Case 3 Update

--- ATM.java
+++ ATM.java
@@ -1,7 +1,37 @@
 import java.util.Scanner;
 
 public class ATM {
+
+    static Scanner scan = new Scanner(System.in);
+    static String[][] infoAkun = {
+            { "Ekal", "Moch. Haikal Putra Muhajir" },
+            { "Alya", "Alya Ajeng Ayu" },
+            { "Erfin", "Erfin Jauhari Dwi Brian" }
+    };
+    static int[][] nominal = {
+            { 332005, 1000000, 23417601 },
+            { 181104, 1000000, 23417602 },
+            { 170405, 1000000, 23417603 }
+    };
+    static boolean menu = false;
+    static boolean online = true;
+    static int user = -1;
+
     public static void main(String[] args) {
+        while (online) {
+            System.out.println("---------------------------------");
+            System.out.println("  SELAMAT DATANG DI BANK JALI !");
+            System.out.println("---------------------------------");
+            while (!menu) {
+                Login();
+            }
+            while (menu) {
+                Menu();
+            }
+        }
+    }
+
+    static void Menu() {
         System.out.println("====================================");
         System.out.println("     ||======================||");
         System.out.println("     ||     PILIHAN MENU     ||");
@@ -23,6 +53,8 @@ public class ATM {
             case 1:
             case 2:
             case 3:
+                SetorTunai();
+                break;
             case 4:
             case 5:
             case 6:
@@ -30,5 +62,46 @@ public class ATM {
             case 8:
             default:
         }
+    }
+
+    static int Login() {
+        System.out.print("Masukkan ID Anda  : ");
+        String inputId = scan.next();
+        System.out.print("Masukkan PIN Anda : ");
+        int inputPin = scan.nextInt();
+        scan.nextLine();
+        for (int i = 0; i < infoAkun.length; i++) {
+            if (inputId.equals(infoAkun[i][0]) && inputPin == nominal[i][0]) {
+                user = i;
+                System.out.println("------------------------------");
+                System.out.println("\tLogin Berhasil !");
+                System.out.println("------------------------------");
+                menu = true;
+            }
+        }
+        if (!menu) {
+            System.out.println("------------------------------");
+            System.out.println("\tLogin Gagal !");
+            System.out.println("------------------------------");
+        }
+        return user;
+    }
+
+    static int SetorTunai() {
+        System.out.println("-----------------------------------------------------");
+        System.out.print("Jumlah uang yang ingin Anda setorkan : Rp");
+        int setor = scan.nextInt();
+        scan.nextLine();
+        if (setor % 50000 == 0) {
+            if(setor > 0){
+            nominal[user][1] += setor;
+            } else {
+            System.out.println("-----------------------------------------------------");
+            System.out.println("Harap masukkan nominal yang valid !");
+            }
+        } else{
+            System.out.println("-----------------------------------------------------");
+        System.out.println("Penyetoran tunai harus kelipatan Rp50.000");}
+        return nominal[user][1];
     }
 }
